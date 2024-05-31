4a88dabfd4998c285eb3dcb9614cba710da84122
amrin4959 <145911614+amrin4959@users.noreply.github.com>
2024-05-31 21:16:23 +0600

    Update Shortest Job First.cpp

--- Shortest Job First.cpp
+++ Shortest Job First.cpp
@@ -1,10 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool compare(const pair<int, int>&i, const pair<int, int>&j)
-{
-    return i.second < j.second;
-}
 int main()
 {
     int n, i, val, temp=0 ;
@@ -13,9 +9,9 @@ int main()
 
     cout << "Enter the Number of  Process : " ;
     cin >> n ;
-    int wt[n], tt[n], et[n], ct[n] ;
+    int wt[n], tt[n], et[n], ct[n], at1[n] ;
 
-    vector<int> processOrder;
+    vector<int> new_order;
 
     cout << "Enter the CPU Times : " << endl ;
     for(i=0; i<n; i++)
@@ -24,48 +20,42 @@ int main()
     cout << "Enter the Arrival Times : " << endl ;
     for(i=0; i<n; i++)
     {
-        cin >> val ;
-        at2.push_back({val, i});
+        cin >> at1[i] ;
+        at2.push_back({at1[i], i});
     }
 
     sort(at2.begin(), at2.end());
 
     priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-    pq.push({ct[at2[0].second],at2[0].first,at2[0].second});
+    pq.push({ct[at2[0].second], at2[0].first, at2[0].second});
     i=1;
     int current_time=0;
 
     while(!pq.empty())
     {
         int burst_time = pq.top()[0];
-        int id = pq.top()[2];
+        int index = pq.top()[2];
         pq.pop();
-        processOrder.push_back(id);
-        current_time= current_time+burst_time;
+        new_order.push_back(index);
+        current_time= current_time + burst_time;
 
         while(true)
         {
             if(i<n && at2[i].first<=current_time)
             {
-                pq.push({ct[at2[i].second],at2[i].first,at2[i].second});
+                pq.push({ct[at2[i].second], at2[i].first, at2[i].second});
                 i++;
             }
             else break;
         }
     }
 
-    temp =0;
-    for(int i=0; i<n; i++)
+    for(i=0; i<n; i++)
     {
-        temp = temp+ct[processOrder[i]];
-        et[processOrder[i]] = temp;
-    }
-    sort(at2.begin(),at2.end(),compare);
-
-    for(int i=0; i<n; i++)
-    {
-        tt[i] = (et[i]-at2[i].first);
-        wt[i] = (tt[i]-ct[i]);
+        et[new_order[i]] = temp + ct[new_order[i]] ;
+        temp = et[new_order[i]] ;
+        tt[new_order[i]] = et[new_order[i]] - at1[new_order[i]] ;
+        wt[new_order[i]] = tt[new_order[i]] - ct[new_order[i]] ;
     }
 
     for(i=0; i<n; i++)
@@ -80,4 +70,3 @@ int main()
     printf("\nAverage Waiting Time : %.2lf \n", awt/n );
     printf("Average Turnaround Time : %.2lf \n", att/n );
 }
-
